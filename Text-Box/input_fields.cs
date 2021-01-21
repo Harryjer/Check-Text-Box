3d949a890ef9072b23b7b5e6e2f03a01ca0e067b
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2021-01-21 21:24:59 +0530

    Add project files.

---
+++ Text-Box/input_fields.cs
@@ -0,0 +1,27 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+
+class input_fields
+{
+
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+        string url = "https://testing.todorvachev.com/special-elements/text-input-field/";
+        driver.Navigate().GoToUrl(url);
+
+        IWebElement element = driver.FindElement(By.Name("username"));
+
+        element.SendKeys("test text");
+
+        Thread.Sleep(3000);
+
+        System.Console.WriteLine(element.GetAttribute("value"));
+
+        Thread.Sleep(3000);
+        driver.Quit();
+    }
+}
